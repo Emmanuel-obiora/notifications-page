73f5e65555478542e2e4e7080997b747aef3b27f
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-04-15 09:36:54 +0100

    Importing frontend mentor's template and structuring development envirenmet

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Your Name Here</a>.
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        Notifications 3
+
+Mark all as read
+    </div>
+  )
+}
+
+export default Header
