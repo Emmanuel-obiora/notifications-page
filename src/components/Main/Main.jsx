86d73438994e45cdba227f14c7a72182eb7bc268
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-04-21 10:47:09 +0100

    Completing notification interaction

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,46 +1,88 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Main.css'
-import Data from './data.json'
 import {BsFillCircleFill} from 'react-icons/bs'
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
 
 const Main = () => {
+  const [notification, setNotification] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getNotification = () => {
+    setIsLoading(true);
+    fetch('http://localhost:3002/db').then((data) => {
+      // console.log(data);
+      return data.json();
+        }).then((response) => {
+            // console.log(response);
+            setNotification(response.notify)
+            setIsLoading(false)
+        }).catch((err) =>{
+            // console.log(err);
+            setIsLoading(false);
+        });
+  }
+
+  useEffect(() => {
+      getNotification();
+       // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+    const markAllUnread = ('click', ()=> {
+      setNotification((prev) => prev.map(n => (
+        {...n, isUnread:false}
+      )))
+    });
+
+    function handleNotificationClick(id){
+      setNotification((prev) => prev.map(n => (n.id === id? 
+        {
+          ...n, isUnread: false
+        }: n)))
+    }
+
 
   return (
     <main className='main-container'>
       <section>
         <div className="left-header">
           <h1>Notifications</h1>
-          <span>3</span>
+          <span>{notification.filter(n => n.isUnread).length}</span>
         </div>
-        <button type='button' className='mark-read'>Mark all as read</button>
+        <button type='button' id='mark' onClick={markAllUnread} className='mark-read'>Mark all as read</button>
       </section>
 
       <div className="messages">
         {
-          Data.map((info, i) => {
+          notification.map((info) => {
             return(
-              <article className='messages-art'>
+              <article className='messages-art' key={info.id} onClick={()=> 
+                handleNotificationClick(info.id)} data-unread={info.isUnread}>
                 <div className='messages-cont'>
                   <img src={info.icon} className="user-icon" alt="" />
                   <div className="messages-cont_info">
                     <h2 className="msg">{info.messenger} 
                       <span className="paragraph">{info.highlight}</span>
                       <span className='notice' id={info.classD}>{info.update}</span>
-                      <BsFillCircleFill className='red-notice'/>
+                      {info.isUnread &&(
+                        <BsFillCircleFill className='red-notice'/>
+                      )}
                     </h2>
                     <span>{info.time}</span>
                   </div>
                   <img src={info.trfImage} className='trf-image' alt="" />
                 </div>
-                <div className='msg-main'>
-                    <p>{info.Msg}</p>
-                  </div>
+                {info.msgUnread && (
+                  <p className='msg-main'>{info.Msg}</p>
+                )}
               </article>
             )
           })
         }
         
       </div>
+      {isLoading && (
+        <LoadingSpinner />
+        )}
     </main>
   )
 }
